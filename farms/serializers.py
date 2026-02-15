8f7b50463b65524c608176ab303fa24aac6f3172
Emad <emadahmedc16@gmail.com>
2026-02-15 01:27:46 +0000

    Added read only fields to farm and product serializers

--- farms/serializers.py
+++ farms/serializers.py
@@ -6,9 +6,11 @@ class FarmSerializer(serializers.ModelSerializer):
     class Meta:
         model = Farm
         fields = "__all__"
+        read_only_fields = ["owner"]
 
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = "__all__"
+        read_only_fields = ["farm"]
