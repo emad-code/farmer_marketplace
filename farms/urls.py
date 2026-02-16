597ff66ca5f711a55c416ed3d66be25d3ea02ea4
Emad <emadahmedc16@gmail.com>
2026-02-16 17:28:20 +0000

    Added Product API routes as views

--- farms/urls.py
+++ farms/urls.py
@@ -1,7 +1,19 @@
 from django.urls import path
-from .views import MyFarmView, MyProductsView
+from .views import (
+    MyFarmView,
+    MyProductsView,
+    CreateProductView,
+    UpdateProductView,
+    DeleteProductView,
+)
 
 urlpatterns = [
+    # FARM
     path("my-farm/", MyFarmView.as_view()),
-    path("my-products/", MyProductsView.as_view()),
+
+    # PRODUCTS
+    path("products/", MyProductsView.as_view()),
+    path("products/create/", CreateProductView.as_view()),
+    path("products/<int:pk>/update/", UpdateProductView.as_view()),
+    path("products/<int:pk>/delete/", DeleteProductView.as_view()),
 ]
