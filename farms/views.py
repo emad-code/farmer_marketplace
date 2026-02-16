6eae556e4d2b0297f782613d065e7f873dac254b
Emad <emadahmedc16@gmail.com>
2026-02-16 17:15:16 +0000

    Added product API views endpoints to Create, Read, Update and Delete products.

--- farms/views.py
+++ farms/views.py
@@ -16,9 +16,33 @@ class MyFarmView(generics.ListCreateAPIView):
         serializer.save(owner=self.request.user)
 
 
+# LIST — Get all products belonging to logged-in farmer
 class MyProductsView(generics.ListCreateAPIView):
     serializer_class = ProductSerializer
     permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
         return Product.objects.filter(farm__owner=self.request.user)
+
+# CREATE — Add product to your farm
+class CreateProductView(generics.CreateAPIView):
+    serializer_class = ProductSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        farm = Farm.objects.filter(owner=self.request.user).first()
+        serializer.save(farm=farm)
+
+
+# UPDATE — Edit your product
+class UpdateProductView(generics.UpdateAPIView):
+    serializer_class = ProductSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    queryset = Product.objects.all()
+
+
+# DELETE — Remove product
+class DeleteProductView(generics.DestroyAPIView):
+    serializer_class = ProductSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    queryset = Product.objects.all()
